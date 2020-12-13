382caa409daa888697fc40c6ae1ad3712b4f237f
rjsrishabh26 <75184385+rjsrishabh26@users.noreply.github.com>
2020-12-14 03:48:38 +0530

    NodeJs app without CSS

---
+++ app.js
@@ -0,0 +1,29 @@
+var click1 = document.querySelector("#button1");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(input) {
+    return serverURL + "?" + "text=" + input
+}
+
+function errorHandler(error) {
+    console.log("Error", error);
+    alert("Something is wrong with the server! Please try again later")
+}
+
+
+function clickHandler() {
+     var inputText = txtInput.value;
+
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+           })
+        .catch(errorHandler)
+};
+
+click1.addEventListener("click", clickHandler)
